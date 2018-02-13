3a548b8fbe5cf615e520389a196cf9e6df3d6fff
J-M <jean-michel.perraud@csiro.au>
2018-02-14 10:49:58 +1100

    Add introductory vignette

---
+++ R/efts_package.R
@@ -0,0 +1,25 @@
+#' Accessing ensemble forecast time series (EFTS) data set stored in netCDF formats
+#' 
+#' \tabular{ll}{
+#' Package: \tab efts\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.7-0\cr
+#' Date: \tab 2018-02-14\cr
+#' Release Notes: \tab Initial commit to github \cr
+#' License: \tab MIT \cr
+#' }
+#'
+#' Accessing ensemble forecast time series (EFTS) data set stored in netCDF formats, without the need for lower-level ncdf4 operations.
+#' See \code{\link{snc_open}} \code{\link{snc_create}} for code examples to read/write files using package.
+#'
+#' \tabular{lll}{
+#' Version \tab Date \tab Notes \cr
+#' 0.6.x \tab 2017 \tab Non public releases of a package originally used only for unit test purposes \cr
+#'
+#' @name efts-package
+#' @aliases efts
+#' @docType package
+#' @title Accessing ensemble forecast time series (EFTS) data set stored in netCDF formats
+#' @author Jean-Michel Perraud \email{jean-michel.perraud_at_csiro.au}
+#' @keywords package netCDF ensemble forecast time series 
+NULL 
